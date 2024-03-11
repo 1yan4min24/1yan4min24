4dd9ead6262580ea6beda71f06a55d74b3637951
1yan4min24 <162960381+1yan4min24@users.noreply.github.com>
2024-03-11 16:51:11 +0900

    Update iyan Salary.py

--- iyan Salary.py
+++ iyan Salary.py
@@ -1,16 +1,31 @@
-def hitung_gaji(jam_kerja, tarif_per_jam):
-    if jam_kerja <= 40:
-        gaji = jam_kerja * tarif_per_jam
-    else:
-        gaji = (40 * tarif_per_jam) + ((jam_kerja - 40) * tarif_per_jam * 1.5)
-    return gaji
+def hitung_gaji_harian(gaji_pokok):
+    return gaji_pokok / 8
+
+def hitung_gaji_mingguan(gaji_pokok):
+    return gaji_pokok / 48
 
 def main():
-    jam_kerja = float(input("Masukkan jumlah jam kerja: "))
-    tarif_per_jam = float(input("Masukkan tarif per jam: "))
-    
-    gaji = hitung_gaji(jam_kerja, tarif_per_jam)
-    print("Gaji yang diterima adalah:", gaji)
+    nama_karyawan = input("Nama karyawan: ")
+    gaji_pokok = float(input("Masukkan gaji pokok: "))
+    jam_kerja_per_hari = 8
+
+    print("\nNama karyawan:", nama_karyawan)
+    print("Gaji pokok:", gaji_pokok)
+    print("Jumlah jam kerja per hari:", jam_kerja_per_hari,"Jam")
+
+    print("\nPilih Rincian Gaji Mingguan/Harian:")
+    print("1. Gaji Harian")
+    print("2. Gaji Mingguan")
+    pilihan = int(input("Pilih [1/2]: "))
+
+    if pilihan == 1:
+        gaji_harian = hitung_gaji_harian(gaji_pokok)
+        print("Gaji harian:", gaji_harian)
+    elif pilihan == 2:
+        gaji_mingguan = hitung_gaji_mingguan(gaji_pokok)
+        print("Gaji mingguan:", gaji_mingguan)
+    else:
+        print("Pilihan tidak valid.")
 
 if __name__ == "__main__":
     main()
